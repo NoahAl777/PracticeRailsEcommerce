d2923e169571987fa0ed6a553c9d83af4bcbe27f
noah <crashlive112@gmail.com>
2021-05-25 23:22:49 -0500

    create /signup costum route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :categories
+  # create custom routes
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  
+  # not custom routes
   resources :comments
   resources :users
-  resources :emails
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
