f614da3cd16212cfd67281aa1c3eb1fba089cb90
noah <crashlive112@gmail.com>
2021-05-25 23:20:43 -0500

    define new and create actions - flip this and last commit message

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   # user created posts
   has_many :posts
   # posts user interacted with
-  has_many :post_buyers, class_name: "Post", foreign_key: :buyer_user_id
+  has_many :post_buyers, class_name: "Post", foreign_key: "buyer_user_id"
   has_many :comments
   has_many :commented_posts, through: :comments, source: :post
   # to determine source you must go to the :through table
+
+  has_secure_password #gives acces to .authenticate and validations
 end
