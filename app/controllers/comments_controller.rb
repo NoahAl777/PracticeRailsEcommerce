598ae21a2b8ed3571c61d3efd3f3bcccac47b93d
noah <crashlive112@gmail.com>
2021-06-02 04:38:14 -0500

    add comment controller methods and views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,45 @@
 class CommentsController < ApplicationController
+  before_action :redirect_if_not_logged_in
+
+  def index
+    @comments = Comment.all
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = current_user.comments.build(comment_params)
+    if @comment.save
+      redirect_to comments_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @comment = Comment.find_by_id(params[:id])
+  end
+
+  def edit
+    @comment = Comment.find_by_id(params[:id])
+  end
+
+  def update
+    @comment = Comment.find_by_id(params[:id])
+    if @comment.update(comment_params)
+      redirect_to comment_path(@comment)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :post_id)
+  end
+  
 end
+
