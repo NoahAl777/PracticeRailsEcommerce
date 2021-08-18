0f630d37fa1f75071cb56b4b6560174bc588cef1
noah <crashlive112@gmail.com>
2021-08-17 21:37:56 -0500

    Add posts controller dynamic routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   # not custom routes
   resources :comments
   resources :users
-  resources :posts
+  resources :posts, only:[:index, :show, :new, :create, :edit, :update]
   resources :sessions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
