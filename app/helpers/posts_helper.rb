d7364a339b518b0cbd71aed5f3e877232f7867b9
noah <crashlive112@gmail.com>
2021-08-27 20:35:08 -0500

    Working on adding buying items function

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,11 @@
 module PostsHelper
+  def buy_item
+    @post = Post.find(params[:id])
+      @post.buyer_user = current_user
+      if @post.accepted_by(current_user)
+        redirect_to @post
+      else
+        render :show
+      end
+    end
 end
